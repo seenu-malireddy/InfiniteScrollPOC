0bc93a5c1c0a1372aa807c6d438c19ab6f84d421
seenu-malireddy <seenu.malireddy@gmail.com>
2025-02-28 12:32:10 +0400

    initial commit

---
+++ products-vite-app/src/App.tsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import InfiniteScrollListView from './components/InfiniteScrollListView'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">Products - Infinite Scrolling</h1>
+        <InfiniteScrollListView />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ products-vite-app/src/components/InfiniteScrollListView.tsx
@@ -0,0 +1,109 @@
+/**
+ * @author Sreenivasulu Malireddy 
+ * Component to show list of products
+ */
+
+import { useEffect, useState, useCallback } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { FixedSizeList as List } from 'react-window';
+import AutoSizer from "react-virtualized-auto-sizer";
+import "../App.css";
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: string;
+  thumbnail: string;
+}
+
+interface ProductResponse {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+// Function to fetch paginated data from API
+const fetchItems = async ({ pageParam = 0 }): Promise<ProductResponse> => {
+  const response = await axios.get<ProductResponse>('https://dummyjson.com/products', {
+    params: { limit: 10, skip: pageParam },
+  });
+  return response.data;
+};
+
+const InfiniteScrollListView = () => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
+    queryKey: ["items"],
+    queryFn: fetchItems,
+    getNextPageParam: (lastPage) => {
+      return lastPage.products.length === lastPage.limit ? lastPage.skip + lastPage.limit : undefined;
+    },
+    initialPageParam: 0, 
+  });
+
+  const [itemCount, setItemCount] = useState(0);
+
+  // Calculate total number of items when data updates
+  useEffect(() => {
+    setItemCount(data?.pages.reduce((total, page) => total + page.products.length, 0) || 0);
+  }, [data]);
+
+  // Callback function to fetch more items when scrolling
+  const loadMoreItems = useCallback(() => {
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
+
+  // Get row data 
+  const getRowData = useCallback((index: number) => {
+    const pageIndex = Math.floor(index / 10);
+    const itemIndex = index % 10;
+    return data?.pages[pageIndex]?.products?.[itemIndex] ?? null;
+  }, [data]);
+
+  return (
+    <div className="list-container">
+    
+      <AutoSizer>
+        {({ height, width }) => (
+          <List
+            height={height} 
+            itemCount={itemCount}
+            itemSize={150} 
+            width={width}
+            onItemsRendered={({ overscanStopIndex }) => {
+              if (hasNextPage && !isFetchingNextPage && overscanStopIndex >= itemCount - 1) {
+                loadMoreItems();
+              }
+            }}
+          >
+            {({ index, style }) => {
+              const item = getRowData(index);
+              return (
+                <div className="list-item" key={item?.id} style={style}>
+                  <div className="w-1/5">
+                    <img
+                      src={item?.thumbnail} 
+                      alt={item?.title}
+                    />
+                  </div>
+                  <div className="flex flex-col w-4/5 p-2">
+                    <p className="text-heading">{item?.title}</p>
+                    <p >{item?.description}</p>
+                    <p className="text-heading">${item?.price}</p>
+                  </div>
+                </div>
+              );
+            }}
+          </List>
+        )}
+      </AutoSizer>
+      {isFetchingNextPage && <p className="loading-text text-center">Loading more products...</p>}
+    </div>
+  );
+};
+
+export default InfiniteScrollListView;
