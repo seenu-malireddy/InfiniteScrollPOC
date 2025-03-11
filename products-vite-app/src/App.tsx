9fe5f53dbbf8c8007e0b360c7b30124d39dbf9cb
seenu-malireddy <seenu.malireddy@gmail.com>
2025-03-11 23:26:41 +0400

    Alternate colors and dark, light theme support added

--- products-vite-app/src/App.tsx
+++ products-vite-app/src/App.tsx
@@ -1,15 +1,27 @@
 
 
+import { useState } from 'react';
 import './App.css'
 import InfiniteScrollListView from './components/InfiniteScrollListView'
 
 function App() {
+
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
   
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    document.documentElement.setAttribute('data-theme', newTheme);
+  };
 
   return (
     <>
       <div className="container">
-        <h1 className="title">Products - Infinite Scrolling</h1>
+      <h1 className="title">Products - Infinite Scrolling</h1>
+      <button onClick={toggleTheme} className="toggle-btn">
+        Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+      </button>
+        
         <InfiniteScrollListView />
       </div>
     </>
